9ba200c187071acffd27f7fd79e253380f111df1
tochibedford <musicofbyte@gmail.com>
2026-08-31 16:21:31 -0700

    Refactor SDL initialization and window management with error handling and rendering logic

--- sdl_window.cpp
+++ sdl_window.cpp
@@ -2,54 +2,102 @@
 #include <SDL3/SDL_main.h>
 #include <stdlib.h>
 
-void cleanup(SDL_Window *win);
 void* buffer_memory;
-int win_width {800};
+SDL_Renderer* renderer;
+SDL_Texture* texture;
+int win_width  {800};
 int win_height {600};
 
-int main(int argc, char *argv[]){
-    if(!SDL_Init(SDL_INIT_VIDEO)){
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error Initializing SDL3", nullptr);
-        return EXIT_FAILURE;
-    }
+static bool init_sdl();
+static SDL_Window* create_window();
+static void resize_buffer(int width, int height);
+static bool process_events();
+static void cleanup(SDL_Window* win);
 
-    
-    SDL_Window *win = SDL_CreateWindow("Simple Game", win_width, win_height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALWAYS_ON_TOP);
-    if(!win){
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", win);
+int main(int argc, char* argv[]) {
+    if (!init_sdl()) return EXIT_FAILURE;
+
+    SDL_Window* win = create_window();
+    if (!win) {
         cleanup(win);
         return EXIT_FAILURE;
     }
 
-    SDL_GetWindowSizeInPixels(win, &win_width, &win_height);
-
-    bool running = true;
-
-    while (running){
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
-                case SDL_EVENT_QUIT: {
-                    running = false;
-                }break;
-                case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED: {
-                    win_width = event.window.data1;
-                    win_height = event.window.data2;
-
-                    int buffer_size = win_width * win_height * sizeof(unsigned int);
-                    if(buffer_memory) free(buffer_memory);
-                    buffer_memory = malloc(buffer_size);
-
-                }break;
-            }
-        }
+    while (process_events()) {
+        // Render goes here.
+        unsigned int* pixel = (unsigned int*)buffer_memory;
+        for (int y = 0; y < win_height; y++)
+            for (int x = 0; x < win_width; x++)
+                *pixel++ = x * y;
+        SDL_UpdateTexture(texture, nullptr, buffer_memory, win_width * sizeof(unsigned int));
+        SDL_RenderTextureRotated(renderer, texture, nullptr, nullptr, 0.0, nullptr, SDL_FLIP_VERTICAL);
+        SDL_RenderPresent(renderer);
     }
-    
+
     cleanup(win);
     return EXIT_SUCCESS;
 }
 
-void cleanup(SDL_Window *win){
+static bool init_sdl() {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
+                                 "Error Initializing SDL3", nullptr);
+        return false;
+    }
+    return true;
+}
+
+static SDL_Window* create_window() {
+    SDL_Window* win = SDL_CreateWindow("Simple Game", win_width, win_height,
+                                       SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALWAYS_ON_TOP);
+    if (!win) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
+                                 "Error creating window", nullptr);
+        return nullptr;
+    }
+
+    renderer = SDL_CreateRenderer(win, nullptr);
+
+  
+    int pixel_width, pixel_height;
+    SDL_GetWindowSizeInPixels(win, &pixel_width, &pixel_height);
+    resize_buffer(pixel_width, pixel_height);
+
+    return win;
+}
+
+static void resize_buffer(int width, int height) {
+    win_width = width;
+    win_height = height;
+
+    int buffer_size = win_width * win_height * sizeof(unsigned int);
+
+    if (buffer_memory) free(buffer_memory);
+    buffer_memory = malloc(buffer_size);
+
+    if(texture) SDL_DestroyTexture(texture);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_XRGB8888, SDL_TEXTUREACCESS_STREAMING, win_width, win_height);
+}
+
+static bool process_events() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_EVENT_QUIT: {
+                return false;
+            } break;
+
+            case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED: {
+                resize_buffer(event.window.data1, event.window.data2);
+            } break;
+        }
+    }
+    return true;
+}
+
+static void cleanup(SDL_Window* win) {
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     free(buffer_memory);
     SDL_DestroyWindow(win);
     SDL_Quit();
