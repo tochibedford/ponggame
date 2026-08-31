16f680ba88fc9c5d99c2056d432efbe589cc2d89
tochibedford <musicofbyte@gmail.com>
2026-08-31 15:45:22 -0700

    Refactor window creation and buffer management in SDL setup

--- sdl_window.cpp
+++ sdl_window.cpp
@@ -1,9 +1,11 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <iostream>
-#include <unistd.h>
+#include <stdlib.h>
 
 void cleanup(SDL_Window *win);
+void* buffer_memory;
+int win_width {800};
+int win_height {600};
 
 int main(int argc, char *argv[]){
     if(!SDL_Init(SDL_INIT_VIDEO)){
@@ -11,15 +13,15 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    int width {800};
-    int height {600};
-    SDL_Window *win = SDL_CreateWindow("Simple Game", width, height, 0);
-
+    
+    SDL_Window *win = SDL_CreateWindow("Simple Game", win_width, win_height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALWAYS_ON_TOP);
     if(!win){
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", win);
         cleanup(win);
         return EXIT_FAILURE;
     }
+
+    SDL_GetWindowSizeInPixels(win, &win_width, &win_height);
 
     bool running = true;
 
@@ -29,16 +31,26 @@ int main(int argc, char *argv[]){
             switch(event.type){
                 case SDL_EVENT_QUIT: {
                     running = false;
-                } break;
+                }break;
+                case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED: {
+                    win_width = event.window.data1;
+                    win_height = event.window.data2;
+
+                    int buffer_size = win_width * win_height * sizeof(unsigned int);
+                    if(buffer_memory) free(buffer_memory);
+                    buffer_memory = malloc(buffer_size);
+
+                }break;
             }
         }
     }
-
+    
     cleanup(win);
     return EXIT_SUCCESS;
 }
 
 void cleanup(SDL_Window *win){
+    free(buffer_memory);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
