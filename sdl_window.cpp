75d695f6037087981bf8208513726b504fe71d4a
tochibedford <musicofbyte@gmail.com>
2026-08-31 15:14:02 -0700

    Refactor SDL initialization and window creation with error handling

--- sdl_window.cpp
+++ sdl_window.cpp
@@ -1,11 +1,44 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <iostream>
+#include <unistd.h>
+
+void cleanup(SDL_Window *win);
 
 int main(int argc, char *argv[]){
-    SDL_Init(SDL_INIT_VIDEO);
+    if(!SDL_Init(SDL_INIT_VIDEO)){
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error Initializing SDL3", nullptr);
+        return EXIT_FAILURE;
+    }
 
+    int width {800};
+    int height {600};
+    SDL_Window *win = SDL_CreateWindow("Simple Game", width, height, 0);
 
-    // SDL_Quit();
-    // return 0;
+    if(!win){
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", win);
+        cleanup(win);
+        return EXIT_FAILURE;
+    }
+
+    bool running = true;
+
+    while (running){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_EVENT_QUIT: {
+                    running = false;
+                } break;
+            }
+        }
+    }
+
+    cleanup(win);
+    return EXIT_SUCCESS;
+}
+
+void cleanup(SDL_Window *win){
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
