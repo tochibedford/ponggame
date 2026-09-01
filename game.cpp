6d8811f8ad549aa0966529216dff11aae623f590
tochibedford <musicofbyte@gmail.com>
2026-09-01 14:34:05 -0700

    Update player position handling and adjust window dimensions

--- game.cpp
+++ game.cpp
@@ -2,21 +2,26 @@
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
 #define released(b) (!(input->buttons[b].is_down) && input->buttons[b].changed)
 
-float player_pos_x = 0.f;
-float player_pos_y = 0.f;
-
+float player_p, player_vel = 0.f;
+float damping_factor = 10.f;
 
 void simulate_game(INPUT *input, float dt){
     clear_screen(0xff5500);
-    // draw_rect_in_pixels(50, 50, 200, 500, 0x00ff22);
+
+    draw_rect(0, 0, 85, 45, 0xffaa33);
     
-    float speed = 5.f;
-    if(pressed(BUTTON_UP)) player_pos_y += speed;
-    if(pressed(BUTTON_DOWN)) player_pos_y -= speed;
-    if(pressed(BUTTON_LEFT)) player_pos_x -= speed;
-    if(pressed(BUTTON_RIGHT)) player_pos_x += speed;
+    float player_accel = 0.f;
+    if(is_down(BUTTON_UP)) player_accel += 2000;
+    if(is_down(BUTTON_DOWN)) player_accel -= 2000;
+
+    player_accel -= player_vel * damping_factor;
+
+    player_p = player_p + (player_vel * dt) + (player_accel * dt * dt * .5f);
+    player_vel =  player_vel + player_accel * dt;
     
-    draw_rect(player_pos_x, player_pos_y, 1, 1, 0x00ff22);
-    draw_rect(30, 30, 5, 5, 0x00ff22);
-    draw_rect(-20, 20, 8, 3, 0x00ff22);
+    // draw_rect(player_pos_x, player_pos_y, 1, 1, 0x00ff22);
+    draw_rect(0, 0, 1, 1, 0xffffff);
+
+    draw_rect(80, player_p, 2.5, 12, 0xff0000);
+    draw_rect(-80, 0, 2.5, 12, 0xff0000);
 }
--- sdl_window.hpp
+++ sdl_window.hpp
@@ -5,7 +5,7 @@
 #include <SDL3/SDL_main.h>
 
 struct Render_State {
-    int win_width {800}, win_height{600};
+    int win_width {1000}, win_height{500};
     SDL_Renderer* renderer;
     SDL_Texture* texture;
     void* buffer_memory;
